2b7ff36f31e5db5e96461757b1980e3fcc3e6883
Vik Paruchuri <vik.paruchuri@gmail.com>
2015-05-08 14:03:21 -0500

    Add 2048

--- js/game_manager.js
+++ js/game_manager.js
@@ -167,7 +167,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 8) self.won = true;
+          if (merged.value === 16) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
